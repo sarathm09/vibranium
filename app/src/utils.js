05118f644dabfa9d87277973cc23988f0bed30b6
Sarath S Menon <sarathm09@gmail.com>
2019-12-16 01:20:46 +0530

    [add] vm2 sandbox code execution logic

--- app/src/utils.js
+++ app/src/utils.js
@@ -1,6 +1,8 @@
-const { scenariosPath } = require('./constants')
+const { VM } = require('vm2')
+const { vibPath, userConfig } = require('./constants')
 const { sep } = require('path')
-const { readFile } = require('fs').promises
+const { readFile, unlink } = require('fs').promises
+const { existsSync, mkdirSync, writeFileSync } = require('fs')
 
 
 /**
@@ -8,7 +10,7 @@ const { readFile } = require('fs').promises
  * @param {string} fileToParse 
  * @returns {string}
  */
-module.exports.getCollectionNameForScenario = fileToParse => fileToParse.replace(scenariosPath, '').split(sep)[1]
+module.exports.getCollectionNameForScenario = fileToParse => fileToParse.replace(vibPath.scenarios, '').split(sep)[1]
 
 
 /**
@@ -16,7 +18,7 @@ module.exports.getCollectionNameForScenario = fileToParse => fileToParse.replace
  * @param {string} fileToParse 
  * @returns {string}
  */
-module.exports.getScenarioFileNameFromPath = fileToParse => fileToParse.replace(scenariosPath, '').split(sep).pop().split('.')[0]
+module.exports.getScenarioFileNameFromPath = fileToParse => fileToParse.replace(vibPath.scenarios, '').split(sep).pop().split('.')[0]
 
 
 /**
@@ -33,7 +35,7 @@ module.exports.splitAndTrimInput = input => input ? input.split(',').map(_ => _.
  * @param {string} input 
  * @returns {boolean}
  */
-module.exports.isAll = input => input ? input.toLocaleLowerCase() === 'all' : true
+module.exports.isAll = input => input ? input.toLowerCase() === 'all' : true
 
 
 /**
@@ -54,7 +56,7 @@ module.exports.sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 module.exports.includesRegex = (arr, input) => arr.filter(_ => input.toLowerCase().match(_.toLowerCase())).length > 0
 
 
-module.exports.parseJsonFile = (fileToParse, fileData, payload) => {
+const parseJsonFile = async (fileToParse, fileData, payload) => {
     let fileParseStatus = {};
     try {
         let obj = JSON.parse(fileData);
@@ -62,16 +64,16 @@ module.exports.parseJsonFile = (fileToParse, fileData, payload) => {
         if (!payload) {
             obj = {
                 ...obj,
-                file: fileToParse.replace(scenariosPath, ''),
+                file: fileToParse.replace(vibPath.scenarios, ''),
                 collection: module.exports.getCollectionNameForScenario(fileToParse),
-                sid: `${fileToParse.replace(scenariosPath, '').split(sep)[1]}_${obj.name}`
+                sid: `${fileToParse.replace(vibPath.scenarios, '').split(sep)[1]}_${obj.name}`
             }
         }
         fileParseStatus = { status: true, data: obj };
     } catch (error) {
         fileParseStatus = { status: false, message: `Parsing file ${fileToParse} failed with the error ${error}` };
     }
-    return fileParseStatus;
+    return fileParseStatus
 }
 
 
@@ -90,6 +92,85 @@ module.exports.readJsonFile = async (fileToParse, payload = false) => {
 
     else {
         let fileData = await readFile(fileToParse, 'utf8');
-        return module.exports.parseJsonFile(fileToParse, fileData, payload);
+        return parseJsonFile(fileToParse, fileData, payload);
     }
+}
+
+
+/**
+ * Save the scenarios into cache. 
+ * Cache is created by running --freeze option
+ * 
+ * @param {array} scenarios List of scenarios
+ */
+module.exports.freezeScenarios = scenarios => {
+    if (!existsSync(vibPath.cache)) {
+        mkdirSync(vibPath.cache)
+    }
+    writeFileSync(vibPath.cachedScenarios, JSON.stringify(scenarios))
+}
+
+
+/**
+ * Remove all cache. Triggered by the --unfreeze option
+ */
+module.exports.unfreezeScenarios = async () => {
+    await unlink(vibPath.cachedScenarios)
+}
+
+
+/**
+ * Check if cache exists. Cache is created by running --freeze option
+ */
+module.exports.cacheExists = () => existsSync(vibPath.cachedScenarios)
+
+
+/**
+ * Load the cached scenarios
+ */
+module.exports.loadCachedScenarios = async () => {
+    let cachedScenarios = await readFile(vibPath.cachedScenarios, 'utf8')
+    return JSON.parse(cachedScenarios)
+}
+
+
+/**
+ * Get available systems defined in the config
+ * TODO: system and default keys
+ */
+module.exports.getAvailableSystemsFromConfig = () => {
+    return {
+        systems: userConfig.systems,
+        default: userConfig.defaultSystem
+    }
+}
+
+
+/**
+ * Execute a given script and return the variables
+ * 
+ * @param {string} script The script to execute
+ * @param {object} variables The variables to be used for script execution and also for setting env vars from script
+ * @param {logger} logger object to log details
+ * @param {function} getApiResponse method to get API response. Used inscripts to get api response.
+ * @returns {object} modified variables
+ */
+module.exports.executeScript = (script, getApiResponse, variables, logger) => {
+    const vm = new VM({
+        external: true,
+        sandbox: {
+            variables,
+            getApiResponse,
+            logger
+        }
+    });
+    const scriptName = 'usersScript_' + uuid4().replace(/-/g, '');
+    vm.run(`
+        const ${scriptName} = () => {
+            ${script}
+        }
+        logger.debug('Executing script ${scriptName}')
+        ${scriptName}()
+    `)
+    return variables
 }

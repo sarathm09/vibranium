a82be82dad4ae6551d7fdabe9ff747fc38551ede
Sarath S Menon <sarathm09@gmail.com>
2020-04-05 19:18:10 +0530

    [fix] log rotation issue

--- app/src/logger.js
+++ app/src/logger.js
@@ -4,9 +4,8 @@ const { readdir, unlink, rmdir } = require('fs').promises
 const { createWriteStream } = require('fs')
 
 const logHandler = require('./loghandler');
-const { userConfig, vibPath, colorCodeRegex,
-	logRotationConstants, logLevels } = require('./constants');
-
+const { userConfig, vibPath, colorCodeRegex, logRotationConstants, 
+	logLevels} = require('./constants');
 
 const getDefaultLogLevel = () => {
 	let logLevel = 'info';
@@ -21,29 +20,33 @@ const getDefaultLogLevel = () => {
 
 
 const rotateOldLogFiles = async () => {
-	let rotationMills = 10 * 24 * 60 * 60; // 10 days
-	const today = new Date()
-	const logFileTimeStamp = new Date(today.getFullYear(), today.getMonth(), today.getDate()).getTime() / 1000
+	try {
+		let rotationMills = 10 * 24 * 60 * 60; // 10 days
+		const today = new Date()
+		const logFileTimeStamp = new Date(today.getFullYear(), today.getMonth(), today.getDate()).getTime() / 1000
 
-	if (!!userConfig.logger && !!userConfig.logger.max_log_history_to_keep) {
-		const rotationTime = userConfig.logger.max_log_history_to_keep;
-		for (const key in Object.keys(logRotationConstants)) {
-			if (rotationTime.includes(key) && !isNaN(rotationTime.replace(key, ''))) {
-				rotationMills = parseInt(rotationTime.replace(key, '')) * logRotationConstants[key]
-				break
+		if (!!userConfig.logger && !!userConfig.logger.max_log_history_to_keep) {
+			const rotationTime = userConfig.logger.max_log_history_to_keep;
+			for (const key in Object.keys(logRotationConstants)) {
+				if (rotationTime.includes(key) && !isNaN(rotationTime.replace(key, ''))) {
+					rotationMills = parseInt(rotationTime.replace(key, '')) * logRotationConstants[key]
+					break
+				}
 			}
 		}
-	}
 
-	let logFiles = await readdir(vibPath.logs)
-	const lastLogFileToKeep = logFileTimeStamp - rotationMills
+		let logFiles = await readdir(vibPath.logs)
+		const lastLogFileToKeep = logFileTimeStamp - rotationMills
 
-	for (const fileName of logFiles) {
-		const filePaths = fileName.split('_')
-		if (filePaths.length > 2 && filePaths[0] === 'log' &&
-			!isNaN(filePaths[1]) && parseInt(filePaths[1]) < lastLogFileToKeep) {
-			await unlink(join(vibPath.logs, fileName))
+		for (const fileName of logFiles) {
+			const filePaths = fileName.split('_')
+			if (filePaths.length > 2 && filePaths[0] === 'log' &&
+				!isNaN(filePaths[1]) && parseInt(filePaths[1]) < lastLogFileToKeep) {
+				await unlink(join(vibPath.logs, fileName))
+			}
 		}
+	} catch (error) {
+		console.error(error)
 	}
 	return
 }
@@ -62,7 +65,7 @@ const rotateOldJobLogs = async () => {
 		}
 		return
 	} catch (error) {
-		console.log(error)
+		console.error(error)
 		return
 	}
 }
@@ -76,14 +79,13 @@ const logData = (moduleName, level, logStream) => async (message, error) => {
 	}
 	if (logLevels[env.LOG_LEVEL] >= logLevels[level] && !env.SILENT)
 		console.log(`[${logHandler.prettyPrint('loglevel', consoleLevel)}] [${consoleModule}]: ${message || ''}`)
-	logStream.write(`[${level}] [${moduleName}] ${Date.now()}: ${message ? message.replace(colorCodeRegex, '') : ''}\n`)
+	logStream.write(`[${consoleLevel}] [${moduleName}] ${Date.now()}: ${message ? message.replace(colorCodeRegex, '') : ''}\n`)
 	if (error) {
 		if ([logLevels.error, logLevels.warn, logLevels.debug].includes(logLevels[env.LOG_LEVEL]))
-			console.log(`[${logHandler.prettyPrint('loglevel', consoleLevel)}] [${consoleModule}]: ${error.stack || 'no stack trace'} `)
+			console.log(`[${consoleLevel}] [${consoleModule}]: ${error.stack || 'no stack trace'} `)
 		logStream.write(error.stack ? error.stack : 'no_stack_trace')
 	}
 }
-
 
 /**
  * Create the logger object to print logs.
@@ -103,6 +105,7 @@ module.exports = (moduleName) => {
 		info: logData(moduleName, 'info', logStream),
 		warn: logData(moduleName, 'warn', logStream),
 		debug: logData(moduleName, 'debug', logStream),
-		error: logData(moduleName, 'error', logStream)
+		error: logData(moduleName, 'error', logStream),
+		success: logData(moduleName, 'success', logStream)
 	}
 }

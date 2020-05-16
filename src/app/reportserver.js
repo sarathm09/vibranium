929c03198d82fa0ec15f03ed1bbf6a66f9437518
Sarath S Menon <sarathm09@gmail.com>
2020-05-16 16:20:17 +0530

    [update] moved report ui to a new module

--- src/app/reportserver.js
+++ src/app/reportserver.js
@@ -1,7 +1,9 @@
 const open = require('open')
+const cors = require('cors')
 const { join } = require('path')
 const express = require('express')
 const { env } = require('process')
+const { readFile, writeFile, readdir } = require('fs').promises
 const { initializeDatabase, getJobHistory, getAPIExecutionHistory, deleteJobHistory } = require('./dbhandler')
 
 const app = express()
@@ -11,6 +13,16 @@ const port = env.PORT || 1234
  * 404 reponse HTML
  */
 const error404 = () => '<head><style>@import url(https://fonts.googleapis.com/css?family=Inconsolata);html{min-height:100%}body{box-sizing:border-box;height:100%;background-color:#000;background-image:radial-gradient(#11581e,#041607),url(https://media.giphy.com/media/oEI9uBYSzLpBK/giphy.gif);background-repeat:no-repeat;background-size:cover;font-family:Inconsolata,Helvetica,sans-serif;font-size:1.5rem;color:rgba(128,255,128,.8);text-shadow:0 0 1ex #3f3,0 0 2px rgba(255,255,255,.8)}.noise{pointer-events:none;position:absolute;width:100%;height:100%;background-image:url(https://media.giphy.com/media/oEI9uBYSzLpBK/giphy.gif);background-repeat:no-repeat;background-size:cover;z-index:-1;opacity:.02}.overlay{pointer-events:none;position:absolute;width:100%;height:100%;background:repeating-linear-gradient(180deg,rgba(0,0,0,0) 0,rgba(0,0,0,.3) 50%,rgba(0,0,0,0) 100%);background-size:auto 4px;z-index:1}.overlay::before{content:"";pointer-events:none;position:absolute;display:block;top:0;left:0;right:0;bottom:0;width:100%;height:100%;background-image:-webkit-gradient(linear,left bottom,left top,from(transparent),color-stop(2%,rgba(32,128,32,.2)),color-stop(3%,rgba(32,128,32,.8)),color-stop(3%,rgba(32,128,32,.2)),to(transparent));background-image:linear-gradient(0deg,transparent 0,rgba(32,128,32,.2) 2%,rgba(32,128,32,.8) 3%,rgba(32,128,32,.2) 3%,transparent 100%);background-repeat:no-repeat;-webkit-animation:scan 7.5s linear 0s infinite;animation:scan 7.5s linear 0s infinite}@-webkit-keyframes scan{0%{background-position:0 -100vh}100%,35%{background-position:0 100vh}}@keyframes scan{0%{background-position:0 -100vh}100%,35%{background-position:0 100vh}}.terminal{box-sizing:inherit;position:absolute;height:100%;width:1000px;max-width:100%;padding:4rem;text-transform:uppercase}.output{color:rgba(128,255,128,.8);text-shadow:0 0 1px rgba(51,255,51,.4),0 0 2px rgba(255,255,255,.8)}.output::before{content:"> "}a{color:#fff;text-decoration:none}a::before{content:"["}a::after{content:"]"}.errorcode{color:#fff}</style><body><div class="noise"></div><div class="overlay"></div><div class="terminal"> <h1>Error <span class="errorcode">404</span></h1> <p class="output">The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p><p class="output">Please try to <a href="/ui">return to the homepage</a>.</p><p class="output">Good luck.</p></div></body>'
+
+
+const copyReportFiles = async () => {
+    let src = join(__dirname, '..',  '..', 'node_modules', 'vibranium-report-ui', 'dist-ui')
+    let files = await readdir(src)
+    for (const file of files){
+        let contents = await readFile(join(src, file), 'utf-8')
+        await writeFile(join(__dirname, '..', 'res', 'ui', file), contents)
+    }
+}
 
 
 /**
@@ -79,9 +91,11 @@ const responseHandler = (db, req, res, handler) => {
  */
 module.exports = async () => {
     let db = await initializeDatabase()
+    await copyReportFiles()
 
     const uiPage = join(__dirname, '..', 'res', 'ui', 'index.html')
     app.use(express.json())
+    app.use(cors())
     app.use('/res', express.static(join(__dirname, '..', 'res', 'ui')))
 
     app.get('/', (_, res) => res.redirect('/ui'))

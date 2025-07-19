91a4ba40ad0a7fc3dd73c97389bc48876db35f8b
Sarath S Menon <sarathm09@gmail.com>
2025-07-19 08:32:55 +0530

    reset all code, back to drawing board

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const rh = require('./app/requesthandler');
-
-module.exports = {
-    createScenario: rh.handleCreateCommand,
-    listTests: rh.handleListCommand,
-    runTests: rh.handleRunCommand
-}
